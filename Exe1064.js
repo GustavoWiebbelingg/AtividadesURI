49f72132142f1a81a355be6b5f6c8cdf2b4f40f5
GustavoWiebbelingg <gustavo16012003@gmail.com>
2021-08-27 08:27:20 -0300

    Inserção da atividade concluida.

---
+++ Exe1064.js
@@ -0,0 +1,28 @@
+var input = require('fs').readFileSync('./dev/stdin/test.txt', 'utf8');
+var lines = input.split('\r\n')
+
+let valores = []
+let count = 0
+let x = 0
+let y = 0
+somaValores = 0
+somaPositivos = 0
+
+while (x < 6) {
+    valores.push(parseFloat(lines.shift()))
+    x++
+}
+
+while (y < 6) {
+    if (valores[y] > 0) {
+        somaPositivos++
+        somaValores = somaValores + valores[y]
+        count++
+    }  
+    y++
+}
+
+
+
+console.log(`${somaPositivos} valores positivos`)
+console.log((somaValores / count).toFixed(1))
